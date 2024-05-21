7a8308d8745c203d91d3d21eb3df454c90ab9713
Faishal Yusuf Baqir <101534919+faishalyb@users.noreply.github.com>
2024-05-22 04:26:13 +0700

    fix orientation bug

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
 from flask import Flask, request, send_file, jsonify
-from PIL import Image
+from PIL import Image, ExifTags
 import io
 from model import get_prediction, draw_boxes
 
 app = Flask(__name__)
+
+def correct_image_orientation(image):
+    try:
+        for orientation in ExifTags.TAGS.keys():
+            if ExifTags.TAGS[orientation] == 'Orientation':
+                break
+
+        exif = image._getexif()
+        if exif is not None:
+            orientation = exif.get(orientation, 1)
+            if orientation == 3:
+                image = image.rotate(180, expand=True)
+            elif orientation == 6:
+                image = image.rotate(270, expand=True)
+            elif orientation == 8:
+                image = image.rotate(90, expand=True)
+    except (AttributeError, KeyError, IndexError):
+        # cases: image don't have getexif
+        pass
+
+    return image
 
 @app.route("/")
 def main():
@@ -19,15 +40,18 @@ def predict():
     image_format = Image.open(image_bytes).format  # Detect image format
     image_bytes.seek(0)  # Reset the stream to the beginning
 
-    # Resize the image to 300x400 pixels
+    # Correct image orientation
     image = Image.open(image_bytes)
-    image = image.resize((300, 400))
+    image = correct_image_orientation(image)
+    
+    # Resize the image to 300x400 pixels
+    image = image.resize((500, 500))
     image_bytes = io.BytesIO()
     image.save(image_bytes, format=image_format)
     image_bytes.seek(0)  # Reset the stream to the beginning
 
     # Get predictions
-    img, boxes, labels, scores = get_prediction(image_bytes, threshold=0.5)
+    img, boxes, labels, scores = get_prediction(image_bytes, threshold=0.7)
     img_with_boxes = draw_boxes(img, boxes, labels, scores)
 
     # Save the image with boxes to a BytesIO object
